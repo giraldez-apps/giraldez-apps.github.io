99c73c5a0000c5c9639a960e125d94ec73437f6a
Fernando Franco Giráldez <ffrancogiraldez@gmail.com>
2016-11-22 20:21:02 +0100

    updated toc.js

--- youtubers-app/wp-includes/js/toc.js
+++ youtubers-app/wp-includes/js/toc.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
-  if (window.location.pathname && window.location.pathname == '?117') {
-    window.location.assign("/index.html@p=117.html")
+  if (window.location.search && window.location.search == '?page_id=117') {
+    window.location = "index.html@p=117.html"
   } else {
-    console.log("current location path: " + window.location.pathname)      
+    console.log("current location path: " + window.location.pathname)
   }
 })
